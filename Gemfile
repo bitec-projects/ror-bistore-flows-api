640a8d1ea781794f75226555a4e2e65e9dc7ecf2
eduardo.bier <eduardo.bier@bitec.dev.br>
2025-09-19 12:39:44 -0300

    Ajustando rotas de sidekiq

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 gem "sidekiq"
 gem "redis", "~> 4.0"
 gem "dotenv-rails", groups: [ :development, :test ]
+gem "pg", ">= 1.1"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 8.0.2", ">= 8.0.2.1"
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
-namespace :api do
-  namespace :v1 do
-    post "webhooks/evolution", to: "webhooks#evolution"
+require "sidekiq/web"
+
+Rails.application.routes.draw do
+  # Adicione esse bloco só ao redor do mount Sidekiq::Web:
+  if Rails.env.development? || Rails.env.test?
+    # Middleware para sessão/cookies só para o painel Sidekiq local
+    Sidekiq::Web.use ActionDispatch::Cookies
+    Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_session_id"
+    mount Sidekiq::Web => "/sidekiq"
+  end
+
+  # ... suas demais rotas
+  namespace :api do
+    namespace :v1 do
+      post "webhooks/evolution", to: "webhooks#evolution"
+    end
   end
 end
