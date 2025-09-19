f2cc80e68f7ec92983773c8b4f8736c1568f7c65
eduardo.bier <eduardo.bier@bitec.dev.br>
2025-09-19 12:53:18 -0300

    Adicionando proteção painel sidekiq em produção

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  # Adicione esse bloco só ao redor do mount Sidekiq::Web:
-  if Rails.env.development? || Rails.env.test?
-    # Middleware para sessão/cookies só para o painel Sidekiq local
-    Sidekiq::Web.use ActionDispatch::Cookies
-    Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_session_id"
-    mount Sidekiq::Web => "/sidekiq"
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |user, password|
+      # Use um usuário/senha forte só para a produção!
+      ActiveSupport::SecurityUtils.secure_compare(user, "bitec_admin") &
+      ActiveSupport::SecurityUtils.secure_compare(password, "Bitec@2024#Admin2025")
+    end
   end
+
+  # Middleware para sessão/cookies só para o painel Sidekiq local
+  Sidekiq::Web.use ActionDispatch::Cookies
+  Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_session_id"
+  mount Sidekiq::Web => "/sidekiq"
 
   # ... suas demais rotas
   namespace :api do
